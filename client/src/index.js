1722116a943b3756055382a78e91bddeaac027c4
Scott Sperling <scsper@gmail.com>
2017-09-09 13:46:26 -0500

    Basic app with create-react-app + redux.

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import App from './app';
+
+function rootReducer(state, action) {
+  return {};
+}
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
